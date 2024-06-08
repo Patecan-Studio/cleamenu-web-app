99d31947b7de650ecb154be1f7d36777c3e242ab
Trần Thiên Trọng <tranthientrong.it@gmail.com>
2024-06-08 20:09:05 +0700

    init all

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -1,7 +1,7 @@
-import GitHub from "next-auth/providers/github";
-
-import type {NextAuthConfig } from "next-auth"
-
-export default {
-    providers: [GitHub],
-} satisfies NextAuthConfig
+// import GitHub from "next-auth/providers/github";
+//
+// import type {NextAuthConfig } from "next-auth"
+//
+// export default {
+//     providers: [GitHub],
+// } satisfies NextAuthConfig
